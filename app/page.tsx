7995064ca452b2766b82ddd142a9cfb64d2be822
Ahmed Wael <70108576+JAHMD@users.noreply.github.com>
2023-08-18 17:35:52 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-export default function Home() {
+export default async function Home() {
+	const data = await fetch("/api/hello").then((res) => res.json());
+
 	return (
 		<main className="min-h-[calc(100vh-72.8px)] py-16 container">
 			<h1>Home page</h1>
+			<p>Data: {data.hello}</p>
 		</main>
 	);
 }
